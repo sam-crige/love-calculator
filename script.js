ea516fbb8eab9c85dd41e8a77fc56314fa075fb9
Adeniyi samuel <adeniyisamuel414@gmail.co>
2024-08-31 10:58:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const inpBtn = () => {
+    let rad = Math.round(Math.random()*100);
+    //okay.innerHTML= rad
+    if(inpName1.value =='' || inpName2.value ==''){
+        alert('Input Your Name Below')
+    } else {okay.innerHTML = rad};
+} 
